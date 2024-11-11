447c5d07ecef45ff73b9560817148345a9e9af17
Sarvesh-Jaiswal <jaiswal.sarvesh01@gmail.com>
2024-11-11 14:26:48 +0530

    base URL updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
-const baseUrl = "/Project2";
+const baseUrl = "";
 
 // https://vitejs.dev/config/
 export default defineConfig({
